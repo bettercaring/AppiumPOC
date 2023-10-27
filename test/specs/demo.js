8b349558fa404465af28b4e115cef725b7eeab2e
gauravgarv77 <gaurav.singh@mable.com.au>
2023-10-27 11:25:14 +1000

    mable login scenarios

--- test/specs/demo.js
+++ test/specs/demo.js
@@ -1,6 +1,6 @@
 describe('sample test', () => {
-    it('login check', async () => {
-      await driver.pause(8000)
+    it('login chck- forgot password', async () => {
+      await driver.pause(4000)
       const signinButton = await $('//*[@text="Log in"]')
       await expect(signinButton).toHaveText('Log in')
       const forgotButton = await $('//*[@text="Forgot Password"]')
@@ -9,6 +9,32 @@ describe('sample test', () => {
       await $('//*[@text="Next"]').click()
       const errorMessage = await $(`//*[@text= "Email can't be blank"]`)
       await expect(errorMessage).toHaveText("Email can't be blank")
+      await driver.back()
 
     });
+    
+    it('Sign-in flow with Correct Password', async () => {
+     await driver.pause(3000)
+     await $('//*[@hint= "Email"]').setValue("abvRav@donotuse.com")
+     await $('//*[@hint= "Password"]').setValue("Mable2023")
+     const signinButton = await $('//*[@text="Log in"]')
+     await signinButton.click()
+     await driver.pause(6000) 
+     const ClientWelcome = await $(`//*[@text= "Hi Rav"]`)
+     await expect(ClientWelcome).toHaveText("Hi Rav")
+     await $('//*[@text="Client profile Rav Clientfirst Account"]').click()
+     await $('//*[@text="Log Out"]').click()
+     await $('//*[@text="YES"]').click()
+     await driver.pause(3000)  
+   });
+   it('Sign-in flow with Incoorrect Password', async () => {
+    await driver.pause(3000)
+    await $('//*[@hint= "Email"]').setValue("gaurav.jenifer_client@donotuse.com.au")
+    await $('//*[@hint= "Password"]').setValue("Mable2023")
+    const signinButton = await $('//*[@text="Log in"]')
+    await signinButton.click()
+    await driver.pause(3000)     
+ 
+     });
+
 })
